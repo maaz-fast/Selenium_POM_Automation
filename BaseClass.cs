fbaf36a6eeddb92492f28152ddf1388e9ce925c1
maaz-fast <k258210@nu.edu.pk>
2026-02-28 13:59:50 +0500

    first commit

---
+++ BaseClass.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json.Linq;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+
+namespace Selenium_POM_Automation
+{
+    public class BaseClass
+    {
+        public static ChromeDriver chromeDriver;
+
+        public void DriverConfiguration()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--headless");
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("--window-size=1920,1080");
+
+            chromeDriver = new ChromeDriver(options);
+        }
+
+        public void OpenBrowserAndURL()
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data.json");
+
+            if (!File.Exists(filePath))
+            {
+                string projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\"));
+                filePath = Path.Combine(projectRoot, "Data.json");
+
+                if (!File.Exists(filePath))
+                {
+                    projectRoot = Path.GetFullPath(Path.Combine(projectRoot, "..\\"));
+                    filePath = Path.Combine(projectRoot, "Data.json");
+                }
+            }
+
+            if (File.Exists(filePath))
+            {
+                var jsonData = JObject.Parse(File.ReadAllText(filePath));
+                var baseUrl = jsonData["baseUrl"].ToString();
+
+                chromeDriver.Manage().Window.Maximize();
+                chromeDriver.Navigate().GoToUrl(baseUrl);
+            }
+            else
+            {
+                throw new FileNotFoundException($"Data.json file not found! Please check the file at this location: {filePath}");
+            }
+        }
+
+        public void CloseBrowser()
+        {
+            if (chromeDriver != null)
+            {
+                chromeDriver.Quit();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selenium_POM_Automation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Selenium_POM_Automation")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e6ea83b5-c951-4266-a05d-241418a26de6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
